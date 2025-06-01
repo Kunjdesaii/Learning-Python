d7f83b7587026e2293a0fd531eff53a80f2e8ce8
Kunjdesaii <kunjrabari123@gmail.com>
2025-06-01 18:35:39 +0530

    firstOne

---
+++ PracticeOne/firstPrint.py
@@ -0,0 +1,2 @@
+# this is my first python program
+print("""In the end, Kafka's keys to a meaningful life are all about finding our own path and living our lives to the fullest, no matter what obstacles may come our way. So don't be afraid to embrace your individuality, take risks, embrace change, connect with others, and dream big""")
---
+++ PracticeOne/import.py
@@ -0,0 +1,5 @@
+import pyttsx3
+k=pyttsx3.init()
+k.say("kunj soon will be an python developer")
+k.runAndWait()
+
