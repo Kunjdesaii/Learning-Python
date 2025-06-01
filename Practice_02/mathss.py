bd3a3a1961355ff548cddd230378e13f82ba5247
Kunjdesaii <kunjrabari123@gmail.com>
2025-06-01 19:04:38 +0530

    second change

---
+++ Practice_02/mathss.py
@@ -0,0 +1,17 @@
+# adding numbers
+a=10
+b=15
+c=20
+print(a+b+c)
+# remainder
+print(c%a)
+# check type of variable
+z=int(input("enter number:"))
+print("entered num :",z)
+# comparison
+print(a>b)
+print(a<b)
+# find average
+print((a+b+c/3))
+# square
+print(a*a)
