de19f1737a23a068b49f9b05e8b287118d5c787a
Kunjdesaii <kunjrabari123@gmail.com>
2025-06-05 13:03:50 +0530

    4

---
+++ Practice_02/sett.py
@@ -0,0 +1,9 @@
+words={
+    "madad": "help",
+    "dost": "friend",
+    "kitab": "book",
+    "pani": "water",
+    "khushi": "happiness",
+}
+word= input("Enter a word for translation: ")
+print(words[word])
