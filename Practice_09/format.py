a05a5a4f45b5798786839337ad21304a7ef05dce
Kunjdesaii <kunjrabari123@gmail.com>
2025-06-20 18:40:08 +0530

    format

---
+++ Practice_09/format.py
@@ -0,0 +1,7 @@
+name= input("Enter your name: ")
+age= int(input("Enter your age: "))
+num= int(input("Enter phone number: "))
+rank= int(input("Enter your rank: "))
+
+k="Student name is {}, age is {}, phone number is {}, rank is {}".format(name, age, num, rank)
+print(k)
